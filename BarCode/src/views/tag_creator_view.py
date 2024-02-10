6f7e8236c38b6f794b5c4dde081563ce5fabea45
frankjrp <frankjrp@gmail.com>
2024-02-10 02:45:36 -0300

    Aula 02

---
+++ BarCode/src/views/tag_creator_view.py
@@ -0,0 +1,18 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_reponse import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
+
+class TagCreatorView:
+    # Responsability for interacting with HTTP
+
+    def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
+
+        # Lógica de regra de negócio
+        formatted_response = tag_creator_controller.create(product_code)
+
+        # Retorno HTTP
+        return HttpResponse(status_code=200, body=formatted_response)

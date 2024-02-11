35e83b18466acf966ff3337f534c5b813669a980
frankjrp <frankjrp@gmail.com>
2024-02-11 04:12:04 -0300

    Aula 03 - Finalização

--- BarCode/src/main/routes/tag_routes.py
+++ BarCode/src/main/routes/tag_routes.py
@@ -2,13 +2,22 @@ from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
 
+from src.errors.error_handler import handle_errors
+
+from src.validators.tag_creator_validator import tag_creator_validator
+
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=["POST"])
 def create_tags():
-    tag_creator_view = TagCreatorView()
+    reponse = None
+    try:
+        tag_creator_validator(request)
+        tag_creator_view = TagCreatorView()
 
-    http_request = HttpRequest(body=request.json)
-    reponse = tag_creator_view.validate_and_create(http_request)
+        http_request = HttpRequest(body=request.json)
+        reponse = tag_creator_view.validate_and_create(http_request)
+    except Exception as exception:
+        reponse = handle_errors(exception)
 
     return jsonify(reponse.body), reponse.status_code
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Projeto
- Projeto Barcode | Python
+## Barcode | Python
+
+### Criação de código de barras
+
+Ao informar um texto qualquer, a aplicação gera uma imagem PNG contendo um código de barras baseado no texto informado.
+
+O projeto foi desenvolvido durante a semana NLW Expert da [Rocketseat](https://rocketseat.com.br) nos dias 05 a 08/02/2024.
+
+## Bibliotecas utilizadas
+
+- [Virtualenv](https://pypi.org/project/virtualenv/)
+- [Pylint](https://pypi.org/project/pylint/)
+- [Flask](https://pypi.org/project/Flask/)
+- [Python-barcode](https://pypi.org/project/python-barcode/)
+- [Pillow](https://pypi.org/project/pillow/)
+- [Cerberus](https://docs.python-cerberus.org/)
+- [Pytest](https://docs.pytest.org/en/8.0.x/)
